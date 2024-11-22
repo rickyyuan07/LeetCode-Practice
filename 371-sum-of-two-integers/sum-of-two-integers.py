ea5b96ce1305aea599396ddb24730f711457548b
Ricky Yuan <rickyyuan07@gmail.com>
2024-11-22 00:24:16 -0500

    Time: 0 ms (100.00%) | Memory: 16.5 MB (57.30%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        MASK = 0xFFFFFFFF
+        INT_MAX = 0x7FFFFFFF
+
+        while b != 0:
+            # XOR
+            sum_ = (a ^ b) & MASK
+            carry = ((a & b) << 1) & MASK
+            a, b = sum_, carry
+        
+        return a if a <= INT_MAX else ~(a ^ MASK)
