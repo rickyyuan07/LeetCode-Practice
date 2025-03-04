22caa8663bb9fbff7cd67532a43cb9b9c8ee8b61
Ricky Yuan <rickyyuan07@gmail.com>
2025-03-04 01:50:20 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (98.24%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,10 +1,12 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         n = len(nums2)
-        dd = {} # element -> next larger element
-        stack = [] # (index, element)
+        dd = {} # element -> next larger element, hashmap for mapping the answers for nums2
+        stack = [] # (index, element), this stores the position that have not found their "next larger element"
 
         for i in range(n):
+            # Every time the current element is larger than some elements in the monotonic stack,
+            # that means we found an answer for the element with the index
             while stack != [] and stack[-1][1] < nums2[i]:
                 index, element = stack.pop()
                 dd[nums2[index]] = nums2[i]
