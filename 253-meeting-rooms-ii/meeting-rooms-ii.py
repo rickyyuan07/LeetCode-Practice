3e0d71d444047d862063eef4e147a18f5bd31aad
Ricky Yuan <rickyyuan07@gmail.com>
2025-10-16 19:54:41 -0400

    Time: 6 ms (38.56%) | Memory: 19.7 MB (99.45%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # Sort by the the intervals' start time
+        intervals.sort()
+
+        pq = []
+        heapq.heapify(pq)  # min heap storing (end, start)
+        ans = -1
+        for itv in intervals:
+            s, e = itv
+            heapq.heappush(pq, (e, s))
+            while pq and pq[0][0] <= s:
+                heapq.heappop(pq)
+            
+            ans = max(ans, len(pq))
+        
+        return ans
